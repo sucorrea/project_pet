e771e34cfd05e90ec39bf741e0b4f8dcbe34f43d
sueli.oliveira <sueli.oliveira@newconsoftware.com.br>
2023-10-10 17:14:42 -0300

    atualizações

--- tests/addfuncionario.php
+++ tests/addfuncionario.php
@@ -41,15 +41,14 @@ mysqli_query($conn, "COMMIT");
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    
 
     <title>AllPet - Dashboard</title>
 
--- tests/index.php
+++ tests/index.php
@@ -14,8 +14,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+
 
     <title>AllPet | Agenda</title>
 
